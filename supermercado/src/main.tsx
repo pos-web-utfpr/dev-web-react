e60abf14d57732eb77bfc8cac9c7907039fd05b5
Andres <andres.porfirio.tablet@gmail.com>
2026-08-17 19:43:03 -0300

    hotfix: vite config

--- supermercado/src/main.tsx
+++ supermercado/src/main.tsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <MantineProvider theme={mantineTheme}>
       <ModalsProvider>
         <Notifications />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <AppRouter />
         </BrowserRouter>
       </ModalsProvider>
--- supermercado/vite.config.ts
+++ supermercado/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: '/dev-web-react/',
   }),
   defineVitestConfig({
     test: {
